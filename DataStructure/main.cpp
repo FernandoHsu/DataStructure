653ed6be140d99c2c8463b178e0563914d5b7a7f
FernandoHsu <supeman20065168@gmail.com>
2018-07-18 16:44:34 +0800

    Use STL::MAP

--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -8,83 +8,36 @@
 // Website: http://dsalgo.openjudge.cn/201409week2/2/
 
 #include <iostream>
-#include <algorithm>
-#include <vector>
+#include <map>
 using namespace std;
 
-struct Polynomial{
-    int coef;
-    int pow;
-};
-
-void show_polynomial(vector<Polynomial> &poly){
-    for (auto p : poly)
-        cout << "[ " << p.coef << " " << p.pow << " ] ";
-    cout << endl;
-};
-
-void getValue(vector<Polynomial>& poly, int& MAX){
-    
-    while (1) {
-        Polynomial input;
-        cin >> input.coef >> input.pow;
-        if (input.pow<0)
-            break;
-        poly.push_back(input);
-        if (input.pow > MAX)
-            MAX = input.pow;
-    }
-    
-};
-
-void show_result(vector<int>& vec){
-    
-    for (int pow=vec.size()-1 ; pow>=0 ; --pow){
-        if (vec.at(pow)==0)
-            continue;
-        cout << "[ " << vec.at(pow) << " " << pow << " ] ";
-    }
-    cout << endl;
-};
-
-void reconstruct(vector<Polynomial>& tem, vector<int>& poly){
-    for (int pow=0; pow<poly.size(); ++pow) {
-        for (int j=0; j<tem.size(); ++j) {
-            if (pow==tem.at(j).pow)
-                poly.at(pow) += tem.at(j).coef;
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        int coef,pow,count=0;
+        map<int,int> m;  // (key, value) = (pow, coef)
+        
+        while(count!=2&&cin>>coef>>pow){
+            if(pow>=0){// Input not ended
+                if(m.count(pow)==-1)// if inputed not existing, then insert
+                    m.insert(make_pair(pow, coef));
+                else{
+                    m[pow]+=coef;// if inputed is existing, then add the coeficient
+                    if(!m[pow])// if coeficient is zere, then remove it.
+                        m.erase(pow);
+                }
+            }
+            else // end inputed
+                count++;
+        }
+        if(count==2){
+            for(auto i=m.rbegin();i!=m.rend();++i){
+                if (i!=m.rbegin())
+                    cout<<' ';
+                cout<<"[ "<<i->second<<' '<<i->first<<" ]";
+            }
+            cout<<endl;
         }
     }
-    
-};
-
-int main(int argc, const char * argv[]) {
-    vector<vector<int>> result;
-    unsigned int n;
-    cin >> n;
-    
-    
-    for (int i=0; i<n; ++i) {
-        int MAX = -1;
-        vector<Polynomial> tem1, tem2;
-        
-        getValue(tem1, MAX);//show_polynomial(tem1);
-        getValue(tem2, MAX);//show_polynomial(tem2);
-        MAX += 1;// To record the largest numbers
-        
-        vector<int>p1, p2, ans;
-        p1.resize(MAX);
-        p2.resize(MAX);
-        ans.resize(MAX);
-        
-        reconstruct(tem1, p1);tem1.clear();
-        reconstruct(tem2, p2);tem2.clear();
-        transform(p1.begin(), p1.end(), p2.begin(), ans.begin(), plus<int>());
-        result.push_back(ans);
-        
-    }
-    
-    for (int i=0; i<result.size(); ++i)
-        show_result(result.at(i));
-
-    return 0;
 }
